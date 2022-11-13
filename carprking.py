e6d1ee46969f9eebb33f3fef1f6eff91040a89dc
Abhishek Rath <abhi13ster@gmail.com>
2022-11-13 11:29:35 +0530

    githubstatus

---
+++ carprking.py
@@ -0,0 +1,8 @@
+#print("My First Python code")
+#5Earningfortheday=0
+Earningfortheday = input("Enter the earning: ")
+print(Earningfortheday)
+if int(Earningfortheday) > 500:
+    print("profitable")
+else:
+    print("Loss")
